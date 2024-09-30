893563fb12645d8e9de90a78ecd4e60c42434b97
AmenaTalukder <amenaakter9714@gmail.com>
2024-09-30 16:06:44 +0600

    MY projects

---
+++ Tasty-Burger/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Blog from "./Blog";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
